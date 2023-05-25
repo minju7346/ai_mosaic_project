45236f662f3730755d7cda72c09669a408a72422
minju7346 <minju7346@naver.com>
2023-05-26 00:44:13 +0900

    aws test

---
+++ src/main/java/com/project/graduation/GraduationProjectApplication.java
@@ -0,0 +1,14 @@
+package com.project.graduation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class GraduationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/graduation/GraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.graduation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
